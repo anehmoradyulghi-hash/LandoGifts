5ee3912225a8a4646941ce36a590e398e9371e75
anehmoradyulghi-hash <anehmorad.yulghi@gmail.com>
2026-07-28 06:25:00 +0330

    Add files via upload

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,10 @@
 module.exports = {
-  apps: [
-    {
-      name: 'lando-gifts',
-      script: 'src/server.js',
-      cwd: __dirname,
-      autorestart: true,        // اگه به هر دلیلی کرش کرد، خودکار دوباره بالا میاد
-      restart_delay: 3000,
-      max_restarts: 1000,
-      min_uptime: '10s',
-      max_memory_restart: '300M',
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
-  ],
+  apps: [{
+    name: "landogifts-backend",
+    script: "./src/server.js",
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000
+    }
+  }]
 };
